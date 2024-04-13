cb8faa5412869e6886d10e1b1d89ba9416f9e2d1
David H <hypersonicsharkz@gmail.com>
2024-04-13 16:22:03 +0200

    Few objects and interactions

---
+++ Scripts/Book.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Randomize color
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        block.SetColor("_Color", Random.ColorHSV());
+        GetComponent<MeshRenderer>().SetPropertyBlock(block, 0);
+    }
+}
