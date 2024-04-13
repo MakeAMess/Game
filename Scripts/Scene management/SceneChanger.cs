8e02516612deab45bf9de63cdcc68129f9c73bd6
Ryvy <mikkelbjoernmartinsen@gmail.com>
2024-04-13 18:44:48 +0200

    Simple main menu created

---
+++ Scripts/Scene management/SceneChanger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
