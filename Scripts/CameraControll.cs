4e3ee0ef3d38828f21ec232acb3d786270e7a63f
Ryvy <mikkelbjoernmartinsen@gmail.com>
2024-04-13 16:56:09 +0200

    Camera clamping works correct

--- Scripts/CameraControll.cs
+++ Scripts/CameraControll.cs
@@ -18,7 +18,7 @@ public class CameraControll : MonoBehaviour
             float horizontalInput = Input.GetAxis("Mouse X") * rotationspeed * Time.deltaTime;
             transform.Rotate(Vector3.right, verticalInput);
             transform.Rotate(Vector3.up, horizontalInput);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+            transform.eulerAngles = new Vector3(Mathf.Clamp(transform.eulerAngles.x, 10, 60), transform.eulerAngles.y, 0);
         }
     }
 }
