c5ddf69cfd3de870b40e7aabf000a068d902afed
Toby <40370496+BitOfAByte@users.noreply.github.com>
2024-04-13 19:04:49 +0200

    AI will follow players within FOV (hopefully)

--- Scripts/EnemyFollower.cs
+++ Scripts/EnemyFollower.cs
@@ -1,17 +1,54 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.AI;
 
-namespace DefaultNamespace
+public class EnemyFollower : MonoBehaviour
 {
-    public class EnemyFollower
+    private NavMeshAgent _agent;
+    private Transform _playerTransform;
+    private readonly float _fieldOfView = 60f; // Field of view in degrees
+    private readonly Vector3[] _path; // The path the enemy should follow
+    private int _currentPathIndex;
+
+    public EnemyFollower(Vector3[] path)
     {
-        private NavMeshAgent _agent;
-        private Transform Movement;
-        
-        
-        void FixedUpdate()
+        _path = path;
+    }
+
+    private void Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsPlayerInFieldOfView())
         {
-            _agent.SetDestination(Movement.position);
+            _agent.SetDestination(_playerTransform.position);
+        }
+        else
+        {
+            FollowPath();
+        }
+    }
+
+    private bool IsPlayerInFieldOfView()
+    {
+        Vector3 directionToPlayer = _playerTransform.position - transform.position;
+        float angle = Vector3.Angle(transform.forward, directionToPlayer);
+        return angle < _fieldOfView / 2;
+    }
+
+    private void FollowPath()
+    {
+        if (_path.Length == 0)
+            return;
+
+        _agent.SetDestination(_path[_currentPathIndex]);
+
+        if (_agent.remainingDistance < _agent.stoppingDistance)
+        {
+            _currentPathIndex = (_currentPathIndex + 1) % _path.Length;
         }
     }
 }
