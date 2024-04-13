cc921f3f699666d9334ac8a326ba05fe23dceeb4
Tobias Jensen <tjens23@student.sdu.dk>
2024-04-13 15:24:52 +0200

    Score Manager

---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿namespace a;
+
+public class ScoreManager : IScoreManager {
+    float score = 0;
+    
+    
+    override public float GetScore() {
+        return this.score;
+    }
+    
+    override public float SetScore(float score) {
+        return this.score = score;
+    }
+    
+    override public float AddScore(float score) {
+        return this.score += score;
+    }
+    
+    override public float SubtractScore(float score) {
+        return this.score -= score;
+    }
+    
+    override public float ResetScore() {
+        return this.score = 0;
+    }
+}
