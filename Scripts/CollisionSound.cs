76a3d4d0b51ed2568805a802433a255f208ac2b0
David H <hypersonicsharkz@gmail.com>
2024-04-14 13:48:09 +0200

    Everything

--- Scripts/CollisionSound.cs
+++ Scripts/CollisionSound.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class CollisionSound : MonoBehaviour
 {
     public AudioSource collisionSound;
+    public float points;
+    public float magnetude = 5;
+
+    private bool pointsGotten;
    
     private void OnCollisionEnter(Collision collision)
     {
         if (!collisionSound)
             return;
 
-        if (collision.relativeVelocity.sqrMagnitude > 1)
+        if (collision.relativeVelocity.sqrMagnitude > magnetude)
         {
             if (!collisionSound.isPlaying)
                 collisionSound.Play();
+
+            if (!pointsGotten)
+            {
+                pointsGotten = true;
+                ScoreManager.Instance.AddScore(points);
+            }
         }
     }
 }
--- Scripts/Scene management/SceneChanger.cs
+++ Scripts/Scene management/SceneChanger.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void ChangeScene(int index)
+    public static float score;
+
+    public static void ChangeScene(int index)
     {
         SceneManager.LoadSceneAsync(index);
     }
 
-    public void ExitGame()
+    public static void ExitGame()
     {
         Application.Quit();
     }
